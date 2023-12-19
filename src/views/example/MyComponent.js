c6e93439d96bdd36a2ebbcd1fe8f51fdcc6268b9
= <=>
2023-12-19 17:24:59 +0700

    state vid 12

--- src/views/example/MyComponent.js
+++ src/views/example/MyComponent.js
@@ -2,9 +2,33 @@ import React from 'react'
 
 class MyComponent extends React.Component {
 
+    state = {
+        name: 'Gau',
+        chanel: 'Duy Gau'
+    }
+
+    /* 
+        - JSX -> return a block (nếu có 2 khối sẽ lỗi -> bọc vào 1 khối div)
+            + Nếu k muốn bọc thêm khối thì ta có thể dùng khối React.Fragment
+        - Để quy định class cho khối ta dùng className chứ k dùng class
+    */
+
+    handleOnchangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
     render() {
         return (
-            <div>Hello my component Duy</div>
+            <React.Fragment>
+                <div className='name'>
+                    <input value={this.state.name} type='text'
+                        onChange={(event) => this.handleOnchangeName(event)} />
+                    Name: {this.state.name}
+                </div>
+                <div className='chanel'>Chanel: {this.state.chanel}</div>
+            </React.Fragment>
         )
     }
 }
