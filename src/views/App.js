1205d9b9293581c9cf1615d92348051373aa7533
= <=>
2023-12-19 22:05:06 +0700

    Form in React vid17

--- src/views/App.js
+++ src/views/App.js
@@ -14,14 +14,6 @@ function App() {
         <p>
           Hello World with React JS
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <MyComponent />
       </header>
     </div>
--- src/views/example/MyComponent.js
+++ src/views/example/MyComponent.js
@@ -3,32 +3,50 @@ import React from 'react'
 class MyComponent extends React.Component {
 
     state = {
-        name: 'Gau',
-        chanel: 'Duy Gau'
+        firstName: '',
+        lastName: ''
     }
 
     /* 
         - JSX -> return a block (nếu có 2 khối sẽ lỗi -> bọc vào 1 khối div)
-            + Nếu k muốn bọc thêm khối thì ta có thể dùng khối React.Fragment
+            + Nếu k muốn bọc thêm khối thì ta có thể dùng khối React.Fragment hoặc <> </>
         - Để quy định class cho khối ta dùng className chứ k dùng class
     */
 
-    handleOnchangeName = (event) => {
+    handleChnageFirstName = (event) => {
         this.setState({
-            name: event.target.value
+            firstName: event.target.value
         })
+    }
+
+    handleChnageLastName = (event) => {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Check data input: ', this.state)
     }
 
     render() {
         return (
-            <React.Fragment>
-                <div className='name'>
-                    <input value={this.state.name} type='text'
-                        onChange={(event) => this.handleOnchangeName(event)} />
-                    Name: {this.state.name}
-                </div>
-                <div className='chanel'>Chanel: {this.state.chanel}</div>
-            </React.Fragment>
+            <>
+                <div>Hello HTML Form</div>
+                <form action="/action_page.php">
+                    <label for="fname">First name:</label>
+                    <br />
+                    <input type="text" value={this.state.firstName} onChange={(event) => this.handleChnageFirstName(event)} />
+                    <br />
+                    <label for="lname">Last name:</label><br />
+                    <input type="text" value={this.state.lastName} onChange={(event) => this.handleChnageLastName(event)} />
+                    <br />
+                    <br />
+                    <input type="submit" value="Submit"
+                        onClick={(event) => this.handleSubmit(event)} />
+                </form>
+            </>
         )
     }
 }
